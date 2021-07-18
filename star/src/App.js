289f46d6c58733cb9655e92251874e965e31e950
gabrielpasquantonio <64622616+gabrielpasquantonio@users.noreply.github.com>
2021-07-18 19:07:28 -0300

    test

--- star/src/App.js
+++ star/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Game from "./Components/Game";
+import './App.css'
+// STAR MATCH - V9
+
+
+
+const StarMatch = () => {
+	const [gameId, setGameId] = useState(1);
+	return <Game key={gameId} startNewGame={() => setGameId(gameId + 1)}/>;
 }
 
-export default App;
+
+
+export default StarMatch;
